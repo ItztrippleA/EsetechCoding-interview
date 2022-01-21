b3fa99869e9ccf7f1d7170c8a011391322de115c
AdamsAFTJ <adams@aftjdigital.com>
2022-01-21 12:46:53 +0100

    time factor

--- esetech-coding-test/src/components/ItemContainer.js
+++ esetech-coding-test/src/components/ItemContainer.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function ItemContainer() {
-  return <div></div>;
+  useEffect(async () => {
+    const result = await axios
+      .get("https://adaorachi.github.io/esetech-assessment-api/game-data.json")
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
+  return (
+    <div className="ItemContainer">
+      <div className="item-div"></div>
+    </div>
+  );
 }
 
 export default ItemContainer;
